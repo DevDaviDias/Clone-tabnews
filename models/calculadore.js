54381753b9b3db62c30b5caa14553148f00963d6
devdavidias <davitdt206@gmail.com>
2025-07-28 00:53:19 +0000

    adicionado arquivos de teste unitários

--- models/calculadore.js
+++
@@ -1,5 +0,0 @@
-function calcular(n1,n2){
-   return n1 + n2;
-}
-exports.calcular = calcular;
-
--- test/unit/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-const calculadora = require("../../models/calculadore");
-
-test("fazendo uma soma simples valor esperador 1+1 =2" , () => {
-    const resultado = calculadora.calcular(1,1);
-    expect(resultado).toBe(2);
-})
