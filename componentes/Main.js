9c01c50a7da8c5a20bc235218eb6a5e07093fc99
devdavidias <davitdt206@gmail.com>
2025-07-29 19:50:06 +0000

    m

--- componentes/Main.js
+++ componentes/Main.js
@@ -3,7 +3,7 @@ import styles from "../styles/Main.module.css";
 
 function Main(){
   return(
-  <div className={styles.fundo}>
+  <div className={styles.fundo1}>
    <h1 className={styles.titulo}>DESBRAVANDO O MUNDO DO CÓDIGO</h1>
    <h2 className={styles.subtitulo}>Construindo Soluções Digitais com Criatividade e Experiência</h2>
    <p className={styles.paragrafo}> Em construção ...🧑‍🔧🔧</p>
