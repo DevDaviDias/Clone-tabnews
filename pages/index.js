3034c0606018e65513f86390952e0ed8a0e98e68
devdavidias <davitdt206@gmail.com>
2025-09-21 11:48:33 +0000

    .

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import Header from"../componentes/Header";
 import Main from"../componentes/Main";
-import Section from"../componentes/section";
+import Section from"../componentes/Section";
 import Tempo from "../componentes/Tempo";
 import MenuHamburguer from "../componentes/MenuHamberguer";
 
