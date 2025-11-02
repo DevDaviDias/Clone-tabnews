111aa1554df7dc29563e55585cd0da194474025d
DevDaviDias <davitdt206@gmail.com>
2025-11-02 00:55:09 +0000

    atualiza componentes e estilo

--- componentes/Main.js
+++ componentes/Main.js
@@ -21,23 +21,48 @@ function Main() {
       className={`${styles.fundo1} ${darkMode ? styles.dark : ""}`}
       id="sobre"
     >
-      <h2 className={styles.titulo}>Minhas skills</h2>
+      <h2 className={styles.titulo}>Sobre min</h2>
+      <div className={styles.alinhamneto}>
+        <div className={styles.conteudoMain1}>
+          <div className={styles.minhahistoria}>
+            <h2>Minha História</h2>
+            <p>
+              Sou Davi Dias, apaixonado por tecnologia e desenvolvimento web.
+              Atualmente curso Análise e Desenvolvimento de Sistemas, buscando
+              evoluir diariamente como desenvolvedor front-end, com o objetivo
+              de me tornar full stack. Acredito que a programação é uma
+              ferramenta poderosa para transformar ideias em soluções reais, e
+              estou sempre em busca de novos desafios e aprendizado.
+            </p>
+          </div>
+        </div>
+        <div className={styles.alinhamneto2}>
+          <div className={styles.infopessoal}>
+            <h2>informações Pessoais </h2>
+            <p>Localização</p>
+            <p>São Paulo, SP - Brasil</p>
+            <p>Idade</p>
+            <p>25 anos</p>
+            <p>Experiência</p>
+            <p>5+ anos</p>
+            <p>Cafés por dia</p>
+            <p>∞</p>
+          </div>
 
-      <div className={styles.conteudoMain1}>
-        <p className={styles.subtitulo}>
-          Sou Davi Dias, apaixonado por tecnologia e desenvolvimento web.
-          Atualmente curso Análise e Desenvolvimento de Sistemas, buscando
-          evoluir diariamente como desenvolvedor front-end, com o objetivo de me
-          tornar full stack. Acredito que a programação é uma ferramenta
-          poderosa para transformar ideias em soluções reais, e estou sempre em
-          busca de novos desafios e aprendizado.
-        </p>
+          <div className={styles.interesses}>
+            <h2> interesses</h2>
+            <p>Programação</p>
+            <p>Desenvolvimento web e mobile</p>
 
-        <div className={styles.iconCircle}>
-          <FaJs color="#1572B6" size={60} />
-          <FaReact color="#61DAFB" size={60} />
-          <FaGitAlt color="#61DAFB" size={60} />
-          <FaNodeJs color="#1572B6" size={60} />
+            <p>Música</p>
+            <p>Rock, eletrônica e lo-fi</p>
+
+            <p>Games</p>
+            <p>RPG e estratégia</p>
+
+            <p>Café</p>
+            <p>Especialidade em grãos especiais</p>
+          </div>
         </div>
       </div>
     </div>
